afd649774f47163141462eb282fc35ec231bd917
Guilherme Varandas <jgvarandas@gmail.com>
2018-07-01 13:31:53 -0300

    Initial Commit

---
+++ ios-tab-app/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  ios-tab-app
+//
+//  Created by Jose Guilherme Costa Varandas on 01/07/18.
+//  Copyright © 2018 Jose Guilherme Costa Varandas. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
